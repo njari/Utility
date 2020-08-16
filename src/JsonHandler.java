78217991b347fb29c54f94286ee7617e3d718c26
Nubra Jarial <nklamusing@gmail.com>
2020-08-16 13:13:10 +0530

    first commit

---
+++ src/JsonHandler.java
@@ -0,0 +1,21 @@
+
+public final class JsonHandler {
+	
+// set of methods to aid parsing and object creation using json 
+	
+	
+// 1. Parse a json into a HashMap<String, String>  The value String might represent another json. 
+
+	
+// 2. Find a key in a json given the key path (can be one level or multiple levels into the json)
+	
+// 3. Parse a json into an object O. where the object members are the entire content of the json
+	
+// 4. Parse json into list of Objects O. 
+	
+// 5. Write test cases for above.
+	
+
+	
+	
+}
---
+++ src/JsonHandlerTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JsonHandlerTest {
+	
+	public static final String SIMPLE_MAP_JSON = "{\r\n" + 
+			"  \"key1\": \"value1\",\r\n" + 
+			"  \"key2\": \"value2\",\r\n" + 
+			"  \"key3\": \"value3\",\r\n" + 
+			"  \"key4\": \"value4\",\r\n" + 
+			"  \"key5\": \"value5\",\r\n" + 
+			"  \"key6\": \"value6\"\r\n" + 
+			"}";
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+
+
+}
